bf2fa21bfb7feb2b411a8f778b8c32f6d61f78ea
Atm Mushfiq Mahmud <62173091+mmrafid@users.noreply.github.com>
2020-07-23 23:52:40 +0600

    Create email.php

---
+++ FORM_VALIDATION_TASK_03/email.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #00FF00}
+</style>
+</head>
+<body> 
+
+
+<?php
+$emailErr = "";
+$email =  "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	
+	function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+	
+	
+	
+	  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+	
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+  
+  
+
+}
+
+?>
+
+
+<h2>E-mail</h2>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+ E-mail:<br/> <input type="text" name="email" placeholder="Hint: sample@example.com">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+
+
+
+
+
+</body>
+</html>
