987f4d96020c0f6189e4ad9513324e3f8dbce0d0
Atm Mushfiq Mahmud <62173091+mmrafid@users.noreply.github.com>
2020-08-03 13:15:56 +0600

    Update logcheck.php

--- MID_LAB_EXAM/logcheck.php
+++ MID_LAB_EXAM/logcheck.php
@@ -11,29 +11,28 @@
             echo "Username/Password is needed";
         }
         else{
-            if(isset($_COOKIE['userName']) && isset($_COOKIE['password'])){
 
-                if($userName == $_COOKIE['userName'] && $password == $_COOKIE['password']){
+            $Adminfile  = fopen('admin.txt', 'r');
+            $Admindata  = fread($Adminfile, filesize('admin.txt'));
 
-                    setcookie('STATUS', 'OK', time()+3600, '/');
-                    header('location: dashboard.php');
+            while(!feof($file)){
 
-                    $file  = fopen('user.txt', 'r');
-			        $data  = fread($file, filesize('user.txt'));
-                    fclose($data);
-                    
-                    $Adminfile  = fopen('admin.txt', 'r');
-			        $Admindata  = fread($Adminfile, filesize('admin.txt'));
-			        fclose($Admindata);
+                $data = fgets($file);
+                $file  = fopen('user.txt', 'r');
+                $data  = fread($file, filesize('user.txt'));
+                fclose($data);
+                
+               
+                fclose($Admindata);
 
-                    $user = explode('|', $data);
-                    $admin = explode('|', $Admindata);
+                $user = explode('|', $data);
+                $admin = explode('|', $Admindata);
 
-                    if($userName == trim($user[0]) && $password == trim($user[1])){
-				        $_SESSION['status'] = "OK";
-				        header('location: UserHome.html');
-                    }
-                    else if($userName == trim($admin[0]) && $password == trim($admin[1]))
+                if($userName == trim($user[0]) && $password == trim($user[1])){
+                    $_SESSION['status'] = "OK";
+                    header('location: UserHome.html');
+                }
+                else if($userName == trim($admin[0]) && $password == trim($admin[1]))
                     {
                         $_SESSION['status'] = "OK";
                         header('location: AdminHome.html');
@@ -42,6 +41,40 @@
                 else{
 				    header('location: login.php?msg=invalid_username/password');
 			    }
+                
+            }
+
+            if(isset($_COOKIE['userName']) && isset($_COOKIE['password'])){
+
+                if($userName == $_COOKIE['userName'] && $password == $_COOKIE['password']){
+
+                    setcookie('STATUS', 'OK', time()+3600, '/');
+                    header('location: dashboard.php');
+
+                    // $file  = fopen('user.txt', 'r');
+			        // $data  = fread($file, filesize('user.txt'));
+                    // fclose($data);
+                    
+                    // $Adminfile  = fopen('admin.txt', 'r');
+			        // $Admindata  = fread($Adminfile, filesize('admin.txt'));
+			        // fclose($Admindata);
+
+                    // $user = explode('|', $data);
+                    // $admin = explode('|', $Admindata);
+
+                    // if($userName == trim($user[0]) && $password == trim($user[1])){
+				    //     $_SESSION['status'] = "OK";
+				    //     header('location: UserHome.html');
+                    // }
+                //     else if($userName == trim($admin[0]) && $password == trim($admin[1]))
+                //     {
+                //         $_SESSION['status'] = "OK";
+                //         header('location: AdminHome.html');
+
+                //     }
+                // else{
+				//     header('location: login.php?msg=invalid_username/password');
+			    // }
 
 
                 }else{
